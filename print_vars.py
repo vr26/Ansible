15d5c4ea79732dfe19e566823c7ee2f33f497333
Venkatesh krishnan R <78889190+vr26@users.noreply.github.com>
2024-03-08 19:57:45 +0530

    Update print_vars.py

--- print_vars.py
+++ print_vars.py
@@ -5,4 +5,8 @@ import os
 # Load the vars_temp file
 with open(f'Core_vars_temp_{os.environ["GITHUB_RUN_ID"]}.yml', 'r') as f:
     vars = yaml.safe_load(f)
-    print(vars)
+
+# Print only the variables that start with 'Core'
+for key, value in vars.items():
+    if key.startswith('Core'):
+        print(f'{key}: {value}')
