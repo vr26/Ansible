f99a087fce53d7bde59423053fb7565c76fe9961
Venkatesh krishnan R <78889190+vr26@users.noreply.github.com>
2024-03-08 20:32:06 +0530

    Update print_vars.py

--- print_vars.py
+++ print_vars.py
@@ -2,11 +2,14 @@
 import yaml
 import os
 
+# Get the type from the environment variable
+type = os.getenv('TYPE')
+
 # Load the vars_temp file
-with open(f'Core_vars_temp_{os.environ["GITHUB_RUN_ID"]}.yml', 'r') as f:
+with open(f'{type}_vars_temp_{os.environ["GITHUB_RUN_ID"]}.yml', 'r') as f:
     vars = yaml.safe_load(f)
 
-# Print only the variables that start with 'Core'
+# Print only the variables that start with the given type
 for key, value in vars.items():
-    if key.startswith('Core'):
+    if key.startswith(type):
         print(f'{key}: {value}')
